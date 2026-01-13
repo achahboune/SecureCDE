44ca4456d00bbe49644926b3d406fe54b67109ed
unknown <achahboune@gmail.com>
2026-01-13 23:20:54 +0100

    fix deploy script

--- deploy.bat
+++ deploy.bat
@@ -5,16 +5,6 @@ pushd "%~dp0" >nul
 set "REMOTE=origin"
 set "BRANCH=main"
 
-if not exist ".git" (
-  echo ERROR: This folder is not a git repo.
-  goto done_err
-)
-
-where git >nul 2>&1 || (
-  echo ERROR: git not found in PATH.
-  goto done_err
-)
-
 set "MSG=%~1"
 if "%MSG%"=="" set "MSG=update"
 
@@ -31,12 +21,7 @@ if errorlevel 1 (
 
 echo [3/4] Sync with remote (rebase)...
 git pull --rebase %REMOTE% %BRANCH%
-if errorlevel 1 (
-  echo ERROR: Rebase failed (conflicts). Run:
-  echo   git status
-  echo   resolve files, then: git add -A ^&^& git rebase --continue
-  goto done_err
-)
+if errorlevel 1 goto done_err
 
 echo [4/4] Push to GitHub (Cloudflare Pages auto-deploy)...
 git push %REMOTE% %BRANCH% || goto done_err
